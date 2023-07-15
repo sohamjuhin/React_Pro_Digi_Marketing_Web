ed927cd656ad79d781842b38eab49aa037ce7d31
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:06:47 +0530

    Create Portfolio.js
    add code

---
+++ components/Portfolio.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <p>Here are some of our successful projects and campaigns:</p>
+      <ul>
+        <li>
+          <h3>Client A - Social Media Marketing Campaign</h3>
+          <p>Increased brand awareness and engagement by 30% through targeted social media ads.</p>
+        </li>
+        <li>
+          <h3>Client B - SEO Optimization</h3>
+          <p>Improved search engine rankings, resulting in a 40% increase in organic traffic.</p>
+        </li>
+        {/* Add more projects and campaigns as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Portfolio;
