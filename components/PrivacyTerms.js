4d153a9b702768970d7f4425a9c6e343ffe25c2d
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:11:32 +0530

    Create PrivacyTerms.js
    add code

---
+++ components/PrivacyTerms.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PrivacyTerms = () => {
+  return (
+    <div>
+      <h2>Privacy Policy and Terms of Service</h2>
+      <h3>Privacy Policy</h3>
+      <p>We are committed to protecting your privacy. Read our privacy policy to learn how we handle and protect your personal information.</p>
+      <h3>Terms of Service</h3>
+      <p>By using our website and services, you agree to comply with our terms and conditions. Please review our terms of service for more information.</p>
+    </div>
+  );
+};
+
+export default PrivacyTerms;
