cac8ef97153ae6b604d6ee09d9a48ceaa774affe
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:10:20 +0530

    Create FAQs.js
    add code

---
+++ components/FAQs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FAQs = () => {
+  return (
+    <div>
+      <h2>FAQs</h2>
+      <h3>Q: Can you guarantee top rankings on search engines?</h3>
+      <p>A: While we strive to improve your search engine rankings, guaranteeing top positions is not feasible due to the ever-changing nature of search algorithms.</p>
+      <h3>Q: How long does it take to see results from SEO?</h3>
+      <p>A: SEO results vary based on several factors, including the competitiveness of keywords and the current state of your website. Generally, it takes several months to start seeing noticeable improvements.</p>
+      {/* Add more FAQs as needed */}
+    </div>
+  );
+};
+
+export default FAQs;
