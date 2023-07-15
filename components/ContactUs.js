272f7926666325f50854373d6ebd1135cbb0c741
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:09:54 +0530

    Create ContactUs.js
    add code

---
+++ components/ContactUs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Get in touch with us to discuss your digital marketing needs:</p>
+      <ul>
+        <li>Phone: 123-456-7890</li>
+        <li>Email: info@company.com</li>
+        <li>Address: 123 Main Street, City, State, ZIP</li>
+      </ul>
+      <p>We look forward to hearing from you!</p>
+    </div>
+  );
+};
+
+export default ContactUs;
