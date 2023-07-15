2150cbb7c315e246ce2d07d495124de5f9014705
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:08:28 +0530

    Create Blog.js
    add code

---
+++ components/Blog.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <div>
+      <h2>Blog</h2>
+      <p>Stay updated with our latest insights and trends in the digital marketing industry:</p>
+      <ul>
+        <li>
+          <h3>Article Title 1</h3>
+          <p>Summary of the article...</p>
+        </li>
+        <li>
+          <h3>Article Title 2</h3>
+          <p>Summary of the article...</p>
+        </li>
+        {/* Add more blog articles as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Blog;
