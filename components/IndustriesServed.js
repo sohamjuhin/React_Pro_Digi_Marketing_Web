6cc5e88488464db3e8b6907673ae9e2c9e0d8587
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:07:59 +0530

    Create IndustriesServed.js
    add code

---
+++ components/IndustriesServed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const IndustriesServed = () => {
+  return (
+    <div>
+      <h2>Industries Served</h2>
+      <p>We have expertise in serving various industries:</p>
+      <ul>
+        <li>Healthcare</li>
+        <li>E-commerce</li>
+        <li>Technology</li>
+        {/* Add more industries as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default IndustriesServed;
