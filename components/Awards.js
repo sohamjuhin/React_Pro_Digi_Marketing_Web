ede0b2e4c2b352900c89e2d03917163c84430b3b
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:14:05 +0530

    Create Awards.js
    add code

---
+++ components/Awards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Awards = () => {
+  return (
+    <div>
+      <h2>Awards and Certifications</h2>
+      <p>We are proud to have received recognition and certifications in the digital marketing industry:</p>
+      <ul>
+        <li>Digital Marketing Excellence Award 2021</li>
+        <li>Google Ads Certified Partner</li>
+        {/* Add more awards and certifications as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Awards;
