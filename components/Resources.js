c054e1efc97a48e46ddd0dd0f35b58c67554d959
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:10:54 +0530

    Create Resources.js
    add code

---
+++ components/Resources.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Resources = () => {
+  return (
+    <div>
+      <h2>Resources</h2>
+      <p>Access our free resources and tools to enhance your digital marketing efforts:</p>
+      <ul>
+        <li>
+          <a href="/resources/guide.pdf" target="_blank" rel="noopener noreferrer">Digital Marketing Guide</a>
+        </li>
+        <li>
+          <a href="/resources/checklist.pdf" target="_blank" rel="noopener noreferrer">SEO Checklist</a>
+        </li>
+        {/* Add more resources as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Resources;
