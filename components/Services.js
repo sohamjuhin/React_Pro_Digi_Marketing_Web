c1951f6a5bd40cdd7eaf427ab5df2932f20d46d0
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:04:45 +0530

    Create Services.js
    add code

---
+++ components/Services.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h2>Our Services</h2>
+      <p>We offer a comprehensive range of digital marketing services tailored to your business needs:</p>
+      <ul>
+        <li>
+          <h3>Search Engine Optimization (SEO)</h3>
+          <p>Improve organic visibility and drive more traffic to your website.</p>
+        </li>
+        <li>
+          <h3>Social Media Marketing</h3>
+          <p>Build and engage your audience across various social media platforms.</p>
+        </li>
+        <li>
+          <h3>Pay-Per-Click (PPC) Advertising</h3>
+          <p>Reach your target audience through targeted and measurable advertising campaigns.</p>
+        </li>
+        {/* Add more services as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
