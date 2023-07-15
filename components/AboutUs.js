1e36197d1b1f6ba323c3dceaa51a122834df18c4
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-07-15 22:05:42 +0530

    Create AboutUs.js
    add code

---
+++ components/AboutUs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>We are a dedicated team of digital marketing professionals passionate about helping businesses succeed online.</p>
+      <p>Our mission is to provide innovative and effective strategies that drive results and exceed client expectations.</p>
+      <h3>Our Expert Team</h3>
+      <p>We have a team of experienced professionals specializing in various aspects of digital marketing:</p>
+      <ul>
+        <li>John Doe - SEO Specialist</li>
+        <li>Jane Smith - Social Media Manager</li>
+        {/* Add more team members as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default AboutUs;
